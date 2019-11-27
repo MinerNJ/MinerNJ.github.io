15f79498dcce917092b4bf9ed7f4501a0b26f9db
Nick Miner <Nick.Miner18@gmail.com>
2019-11-26 17:15:20 -0700

    total overall. Starting with file structure, placed iterations 1 and 2 in their own folders with unique HTML/CSS/JS/ structure. Created 7 HMTL pages, 7 CSS pages and 2 JS pages. Went for broke on one push so website would stay up until new version is up. Time to change!

---
+++ JS/Contact.js
@@ -0,0 +1,9 @@
+function hideAndSeek(){
+    let x = document.getElementById("iconsDiv");
+    if(x.style.display === "none"){
+        x.style.display = "flex";
+    } else {
+        x.style.display = "none";
+    }
+}
+;
--- JS/index.js
+++ JS/index.js
@@ -1,73 +1,95 @@
-//Image Carousel
-const picChanger = document.querySelector('.change-about-pics');
-let j = 0;
-
-let imagesArray = [];
-imagesArray[0]=["./HTML/headshot.jpg", "./HTML/ax.jpg", "./HTML/diploma.jpg", "./HTML/smawblast.jpg"]; 
-imagesArray[1]=["./HTML/elephant2.jpg", "./HTML/lift.jpg", "./HTML/TN-OH.jpg", "./HTML/assault.jpg"];
-imagesArray[2]=["./HTML/animals.jpg", "./HTML/rock-climb.jpg", "./HTML/OH-gym.jpg", "./HTML/afghan.JPG"];
-
-picChanger.addEventListener('click', function() {
-  const images = document.querySelectorAll('.actual-pics');
-  if(j > images.length){
-    j = 0;
-  }
-  for(let i = 0; i < images.length; i++) {
-
-        images[i].setAttribute('src', imagesArray[i][j]);
-  }
-  j++;
-});
-
-//Modal Boxes
-const openModal = document.querySelector('.left-box');
-const openSecondModal = document.querySelector('.right-box');
-const closeModal = document.querySelector('.skills-hide-box');
-const closeSecondModal = document.querySelector('.skills-hide-box2');
+var text = document.getElementById('text');
+var text2 = document.getElementById('text2');
+var text3 = document.getElementById('text3');
+var text4 = document.getElementById('text4');
+var text5 = document.getElementById('text5');
+var newDom = '';
+var newDom2 = '';
+var newDom3 = '';
+var newDom4 = '';
+var newDom5 = '';
+var animationDelay = 12;
+var animationDelay2 = 18;
+var animationDelay3 = 24;
+var animationDelay4 = 30;
+var animationDelay5 = 36;
 
 
-openModal.addEventListener('click', () => {
-  toggleShowSkills(document.querySelector('.skills-hide-box'));
-});
-
-openSecondModal.addEventListener('click', () => {
-  toggleShowSkills(document.querySelector('.skills-hide-box2'));
-});
-
-closeModal.addEventListener('click', () => {
-  toggleShowSkills(document.querySelector('.skills-hide-box'));
-});
-
-closeSecondModal.addEventListener('click', () => {
-  toggleShowSkills(document.querySelector('.skills-hide-box2'));
-});
-
-function toggleShowSkills(element){
-  element.classList.toggle('toggle-skills-content');
+for(let i = 0; i < text.innerText.length; i++)
+{
+    newDom += '<span class="char">' + (text.innerText[i] == ' ' ? '&nbsp;' : text.innerText[i])+ '</span>';
 }
 
-//NavBar
-function toggleHide(element) {
-    element.classList.toggle('hide');
+for(let i = 0; i < text2.innerText.length; i++)
+{
+    newDom2 += '<span class="char">' + (text2.innerText[i] == ' ' ? '&nbsp;' : text2.innerText[i])+ '</span>';
 }
 
-function bindNavButtonToNavList() {
-    const navButton = document.querySelector('.nav__button');
-    
-    navButton.addEventListener('click', function() {
-      const navList = document.querySelector('.icon-list');
-      toggleHide(navList);
-    });
+for(let i = 0; i < text3.innerText.length; i++)
+{
+    newDom3 += '<span class="char">' + (text3.innerText[i] == ' ' ? '&nbsp;' : text3.innerText[i])+ '</span>';
+}
 
-    // navButton.addEventListener('click', () => {
-    //     toggleHide(document.querySelector('.top-nav'));
-    //   });
+for(let i = 0; i < text4.innerText.length; i++)
+{
+    newDom4 += '<span class="char">' + (text4.innerText[i] == ' ' ? '&nbsp;' : text4.innerText[i])+ '</span>';
 }
-  
-function run() {
-    bindNavButtonToNavList();
+
+for(let i = 0; i < text5.innerText.length; i++)
+{
+    newDom5 += '<span class="char">' + (text5.innerText[i] == ' ' ? '&nbsp;' : text5.innerText[i])+ '</span>';
 }
-  
-run();
-  
-  
+
+text.innerHTML = newDom;
+var length = text.children.length;
+
+for(let i = 0; i < length; i++)
+{
+    text.children[i].style['animation-delay'] = animationDelay * i + 'ms';
+}
+
+text2.innerHTML = newDom2;
+var length2 = text2.children.length;
+
+for(let i = 0; i < length2; i++)
+{
+    text2.children[i].style['animation-delay'] = animationDelay2 * i + 'ms';
+}
+
+text3.innerHTML = newDom3;
+var length3 = text3.children.length;
+
+for(let i = 0; i < length3; i++)
+{
+    text3.children[i].style['animation-delay'] = animationDelay3 * i + 'ms';
+}
+
+text4.innerHTML = newDom4;
+var length4 = text4.children.length;
+
+for(let i = 0; i < length4; i++)
+{
+    text4.children[i].style['animation-delay'] = animationDelay4 *  i + 'ms';
+}
+
+text5.innerHTML = newDom5;
+var length5 = text5.children.length;
+
+for(let i = 0; i < length5; i++)
+{
+    text5.children[i].style['animation-delay'] = animationDelay5 * i + 'ms';
+}
+
+const gra = function(min, max) {
+    return Math.random() * (max - min) + min;
+}
+
+const init = function(){
+    let items = document.querySelectorAll('section');
+    for (let i = 0; i < items.length; i++){
+        items[i].style.background = randomColor({luminosity: 'light'});
+    }
+    cssScrollSnapPolyfill()
+}
+init();
+
